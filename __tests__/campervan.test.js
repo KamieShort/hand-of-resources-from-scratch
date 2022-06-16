4f3a095f79129252105ccc8d0d28dc5f2e327f65
Kamie Short <kamie.short@gmail.com>
2022-06-16 11:41:18 -0700

    campervan routes, test, controller, model setup, campervan list displaying

---
+++ __tests__/campervan.test.js
@@ -0,0 +1,37 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('test routes for campervans table', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('/campervans should display list of campervans', async () => {
+    const res = await request(app).get('/campervans');
+
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        make: 'Mercedes',
+      },
+      {
+        id: '2',
+        make: 'Dodge',
+      },
+      {
+        id: '3',
+        make: 'Freightliner',
+      },
+      {
+        id: '4',
+        make: 'Ford',
+      },
+    ]);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
