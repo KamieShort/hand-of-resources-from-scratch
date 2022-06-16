15a3b0ff013475c03a7b5f59fe4a65b7d720198f
Kamie Short <kamie.short@gmail.com>
2022-06-16 11:08:10 -0700

    delete test written

--- __tests__/dog.test.js
+++ __tests__/dog.test.js
@@ -60,6 +60,13 @@ describe('test routes for dogs table', () => {
     expect(resp.body.name).toEqual('Little Berry');
   });
 
+  it('DELETE / dog/:id should delete a dog', async () => {
+    const resp = await request(app).delete('/dogs/2');
+    expect(resp.status).toEqual(200);
+    const { body } = await request(app).get('/dogs/2');
+    expect(body).toEqual('');
+  });
+
   afterAll(() => {
     pool.end();
   });
